7253db446d64fbffb06fb1c6bef2edeffd7abdc1
Matteo M <74145047+Matteo0099@users.noreply.github.com>
2023-05-06 15:22:13 +0200

    Update 1

--- main.cpp
+++ main.cpp
@@ -1,29 +1,55 @@
-#include <ESP8266WiFi.h>
-#include <iostream>
- 
-  const char* ap = "ssid";   //your wifi access point name
-  const char* pass = "password"; //your wifi password
-  int wifiStatus;
-  int connectStatus = 0;
-    
+// WiFi credential constants 
+const char* wifiSSID = "ssid";  
+const char* wifiPassword = "password";
+
+// Pin constants
+const int ledPin = LED_BUILTIN;
+const int wifiPin = 2;
+
+// Pin state constants
+const int ledOn = HIGH;
+const int ledOff = LOW;
+const int wifiConnected = HIGH; 
+const int wifiDisconnected = LOW;
+
+// WiFi connection status variable 
+int wifiConnectionStatus = 0;
+
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT), pinMode(2, OUTPUT);
-  WiFi.begin(ap, pass);
+  // Set pin modes
+  pinMode(ledPin, OUTPUT);
+  pinMode(wifiPin, OUTPUT);
+  
+  // Connect to WiFi
+  connectToWifi(); 
 }
-     
+
 void loop() {
-  wifiStatus = WiFi.status();
-  if(connectStatus == 0) { 
-    digitalWrite(LED_BUILTIN, 1), 
-      digitalWrite(2, 0);
-  } //for inverted change 0 to 1 & 1 to 0
-  if(wifiStatus == WL_CONNECTED) { 
-    digitalWrite(2, 1), 
-      digitalWrite(LED_BUILTIN, 0), 
-      connectStatus ++;
-  } else if(connectStatus != 0) {
-    digitalWrite(LED_BUILTIN, 1), 
-      digitalWrite(2, 0); 
-    }
-  delay(1000);
+  // Check WiFi connection status
+  if(wifiConnectionStatus == 0) {
+    digitalWrite(ledPin, ledOn);
+    digitalWrite(wifiPin, wifiDisconnected); 
+  } 
+  
+  else if(wifiConnectionStatus != 0) {
+    digitalWrite(ledPin, ledOff);
+    digitalWrite(wifiPin, wifiConnected);
+  }
+  
+  delay(1000);  
+}
+
+// Connect to WiFi function 
+void connectToWifi() {
+  WiFi.begin(wifiSSID, wifiPassword);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    digitalWrite(ledPin, ledOn);
+    digitalWrite(wifiPin, wifiDisconnected);
+    delay(1000);
+  }
+  
+  digitalWrite(ledPin, ledOff);
+  digitalWrite(wifiPin, wifiConnected);
+  wifiConnectionStatus++;
 }
